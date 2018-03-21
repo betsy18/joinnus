787b19a71f601e3e94bcef6537a6d0aa376a085b
margb13 <marg.b13@hotmail.com>
2018-03-21 02:16:05 -0500

    Añadiendo componente navbar

---
+++ src/lib/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../lib/style/navbar.css";
+
+const Navbar = () => (
+  <nav class="navbar navbar-joinnus p-2">
+    prueba
+  </nav>
+);
+
+export default Navbar;
