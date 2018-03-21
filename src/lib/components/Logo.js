e4c25072d955f0a1c4decc3b143499f5cdb9021e
margb13 <marg.b13@hotmail.com>
2018-03-21 01:47:18 -0500

    Añadiendo componente logo

---
+++ src/lib/components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../assets/images/logo-header.png';
+
+const Logo = () => (
+  <div className="col-12 d-flex justify-content-center p-3">
+    <img className="img-fluid" src={logo} alt="logo" />
+  </div>
+);
+
+export default Logo;
