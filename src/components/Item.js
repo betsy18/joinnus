e968762c55f88e98f4ab4bf688e530bb22f0a25c
Betsy Lara <betsy_dessire@hotmail.com>
2018-03-22 04:51:18 -0500

    Mejorando el footer

---
+++ src/components/Item.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import '../style/style.css'
+import Section from './Section';
+
+
+const Item = () => (
+  // <div className="carousel-item">
+  //   {events.map(event => 
+  //     <img className="d-block w-100" src={event} key={event} alt={event}
+  //     />)}
+  // </div>
+  <div className="mx-auto">
+    <section className="text-center">
+  <div className="container-fluid">
+    <div className="row">
+      {/* <div className="row"> */}
+        <div className="col-xs-12 col-sm-6 col-md-6">
+        <div className="card">
+            <img className="card-img-top" src="https://s3-us-west-2.amazonaws.com/joinnus.com/user/119517/5a80151f9c9a4.jpg" alt="Card image cap"/>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        </div>
+      {/* </div> */}
+      {/* <div className="row"> */}
+        <div className="col-xs-12 col-sm-6 col-md-6">
+        <div className="card">
+            <img className="card-img-top" src="https://s3-us-west-2.amazonaws.com/joinnus.com/user/119517/5a80151f9c9a4.jpg" alt="Card image cap"/>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        </div>
+      {/* </div> */}
+      {/* <div className="row"> */}
+        <div className="col-xs-12 col-sm-6 col-md-6">
+        <div className="card">
+            <img className="card-img-top" src="https://s3-us-west-2.amazonaws.com/joinnus.com/user/119517/5a80151f9c9a4.jpg" alt="Card image cap"/>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        </div>
+      {/* </div> */}
+      {/* <div className="row"> */}
+        <div className="col-xs-12 col-sm-6 col-md-6">
+        <div className="card">
+            <img className="card-img-top" src="https://s3-us-west-2.amazonaws.com/joinnus.com/user/119517/5a80151f9c9a4.jpg" alt="Card image cap"/>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        </div>
+      {/* </div> */}
+      </div>
+  </div>
+  </section>
+  </div>
+  
+);
+
+
+export default Item
